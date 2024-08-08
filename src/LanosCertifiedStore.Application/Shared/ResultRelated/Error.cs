b0ce4dfb50b0099e9c501d7ac5b4077cdc45beb3
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-08-08 15:51:26 +0300

    Equals and opearator == overriden in Error.cs

--- src/LanosCertifiedStore.Application/Shared/ResultRelated/Error.cs
+++ src/LanosCertifiedStore.Application/Shared/ResultRelated/Error.cs
@@ -1,6 +1,6 @@
 ﻿namespace LanosCertifiedStore.Application.Shared.ResultRelated;
 
-public class Error
+public class Error : IEquatable<Error>
 {
     public Error(string code, string message)
     {
@@ -14,4 +14,59 @@ public class Error
 
     public string Code { get; }
     public string Message { get; }
+
+    public bool Equals(Error? other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return Code == other.Code && Message == other.Message;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != this.GetType())
+        {
+            return false;
+        }
+
+        return Equals((Error)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Code, Message);
+    }
+
+    public static bool operator ==(Error? left, Error? right)
+    {
+        if (ReferenceEquals(left, null))
+        {
+            return ReferenceEquals(right, null);
+        }
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Error? left, Error? right)
+    {
+        return !(left == right);
+    }
 }
